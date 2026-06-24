05c2cfb0c8cc0ca4467a13d65e4fd45f31a5043b
nzaman7878 <nuruzzaman31032001@gmail.com>
2026-06-25 02:38:33 +0530

    feat: implement authentication, chat model, and mail service

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
- import "dotenv/config";
- import app from "./src/app.js";
- import connectDB from "./src/config/database.js";
+import "dotenv/config";
+import app from "./src/app.js";
+import connectDB from "./src/config/database.js";
+import dotenv from "dotenv";
 
-    const PORT = process.env.PORT || 3000;
+dotenv.config();
 
-    connectDB()
-    .catch((err) => {
-        console.error("Failed to connect to the database:", err);
-        process.exit(1);
-    });
+const PORT = process.env.PORT || 3000;
 
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+connectDB().catch((err) => {
+  console.error("Failed to connect to the database:", err);
+  process.exit(1);
+});
 
-    
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,20 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-
+import authRouter from "./routes/auth.routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+);
 
+app.use("/api/auth", authRouter);
 
 export default app;
